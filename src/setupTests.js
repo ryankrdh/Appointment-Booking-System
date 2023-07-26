dbaa302148dab84992ecc0010d6de56f46e11fd4
ryankrdh <ryankrdh@gmail.com>
2023-07-26 08:57:20 -0700

    cleaning up and fixing bugs

---
+++ src/setupTests.js
@@ -0,0 +1,77 @@
+<div className="fun-fact-sec" data-aos="fade-right">
+	<div className="single-fun">
+		<span>500</span>
+		<span>+</span>
+		<p>Happy Patients</p>
+	</div>
+	<div className="single-fun sp-fun" data-aos="fade-right">
+		<span>88</span>
+		<span>+</span>
+		<p>Qualified Doctors</p>
+	</div>
+	<div className="single-fun" data-aos="fade-left">
+		<span>25</span>
+		<span>+</span>
+		<p>Years Experience</p>
+	</div>
+	<div className="single-fun sp-fun" data-aos="fade-left">
+		<span>50</span>
+		<span>+</span>
+		<p>Dental Awards</p>
+	</div>
+	<span className="line"></span>
+</div>;
+
+
+.fun-fact-sec {
+	background-color: #ffffff;
+	box-shadow: #00000014 8.135px 18.271px 40px 0px;
+	margin-top: 50px;
+	position: relative;
+	height: 100%;
+	padding: 65px 80px 21px;
+	overflow: hidden;
+}
+
+.single-fun {
+	position: relative;
+	width: 50%;
+	margin-bottom: 45px;
+	float: left;
+	text-align: start;
+}
+
+.sp-fun {
+	padding-left: 60px;
+}
+
+.single-fun span {
+	font-weight: 600;
+	font-size: 24px;
+	line-height: 1;
+	position: relative;
+}
+
+.single-fun p {
+	color: #666;
+	font-weight: 300;
+	line-height: 1;
+	margin-top: 8px;
+}
+
+.line {
+	position: absolute;
+	left: 50%;
+	top: 50%;
+	transform: translate(-50%, -50%);
+	width: 1px;
+	height: 70%;
+	background: #dadada;
+	content: '';
+}
+
+
+
+
+
+
